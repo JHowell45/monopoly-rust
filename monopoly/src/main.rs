70c605df9065de95f7ec44388434402b053182aa
Jacob <jacob@notactivelylooking.com>
2022-12-26 23:23:30 +0000

    basic test game.

--- monopoly/src/main.rs
+++ monopoly/src/main.rs
@@ -1,10 +1,18 @@
 pub mod game;
 
-fn run_game() {
-    let run_game: bool = true;
-    while run_game {
+use game::Game;
 
-    };
+fn run_game() {
+    let mut game = Game::new();
+    game.add_player("John");
+    game.add_player("Jane");
+    game.add_player("Richard");
+    game.add_player("Amy");
+    println!("{:#?}", game);
+    // let run_game: bool = true;
+    // while run_game {
+
+    // };
 }
 
 fn main() {
