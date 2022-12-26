0b960d79301ebd7f20b80c86ad1025ce3366bd83
Jacob <jacob@notactivelylooking.com>
2022-12-26 23:23:40 +0000

    Add player movement hashmap.

--- monopoly/src/game/mod.rs
+++ monopoly/src/game/mod.rs
@@ -1,15 +1,19 @@
 pub mod player;
 pub mod property;
 
+use std::{collections::HashMap};
+use uuid::Uuid;
 use player::Player;
 
+#[derive(Debug)]
 pub struct Game {
-    players: Vec<Player>
+    players: Vec<Player>,
+    player_movements: HashMap<Uuid, u32>,
 }
 
 impl Game {
     pub fn new() -> Self {
-        Self { players: Vec::new() }
+        Self { players: Vec::new(), player_movements: HashMap::new() }
     }
 
     pub fn add_player(&mut self, name: &str) {
